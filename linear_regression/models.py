82ed85dad10fa1a094b60d1c4557ac13297bfa15
JHowell45 <jhowell45@googlemail.com>
2021-04-06 21:39:48 +0100

    added multivariate linear regression model.

--- linear_regression/models.py
+++ linear_regression/models.py
@@ -1,4 +1,5 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from numpy import array
 
 
 @dataclass
@@ -8,3 +9,12 @@ class UnivariateLinearRegressionModel:
 
     def predict(self, feature: float) -> float:
         return (self.gradient * feature) + self.y_axis_shift
+
+
+@dataclass
+class MultivariateLinearRegression:
+    gradients: array = field(default_factory=array)
+    y_axis_shift: float = 0
+
+    def predict(self, features: array) -> float:
+        return sum(self.gradients * features) + self.y_axis_shift
