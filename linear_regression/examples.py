a9264875e43534d7de24c000b3f2171a7860db87
JHowell45 <jhowell45@googlemail.com>
2021-04-09 00:29:19 +0100

    Creating new examples.

--- linear_regression/examples.py
+++ linear_regression/examples.py
@@ -6,10 +6,41 @@ from linear_regression.models import UnivariateLinearRegressionModel
 from linear_regression.parameter_learning import ulr_batch_gradient_descent
 from math import inf
 from matplotlib import pyplot as plt
+from time import time
+from random import randrange
+from pandas import Series
 
 
-def univariate_linear_regression_example():
+def univariate_linear_regression_example(feature_size: int = 100):
     print("\nRunning Univariate Linear Regression Example:\n\n")
+    features = Series(randrange(0, feature_size * 2) for _ in range(feature_size))
+    labels = Series(4 + 3 * feature + randrange(0, 10) for feature in features)
+    print(f"Features shape: {features.shape}")
+    print(f"Labels shape: {labels.shape}")
+
+    plt.clf()
+    plt.plot(features, labels, "bo")
+    plt.xlabel("features")
+    plt.ylabel("labels")
+    plt.savefig(f"./graphs/linear_regression/univariate_example/artificial_data.png")
+
+    s = time()
+    best_parameters = ulr_batch_gradient_descent(
+        features=features, labels=labels, epochs=100000
+    )
+    print(f"Gradient Descent Runtime: {round(time() - s, 2)}s")
+    print(f"Best Parameters: {best_parameters}")
+    print(f"Best MSE Score: {best_parameters['current_mse_score']}")
+    best_model = UnivariateLinearRegressionModel(
+        theta_0=best_parameters["current_theta_0"],
+        theta_1=best_parameters["current_theta_1"],
+    )
+    print()
+    return best_model
+
+
+def univariate_linear_regression_example_2():
+    print("\nRunning Univariate Linear Regression Example 2:\n\n")
     features, labels = load_boston(return_X_y=True)
     print("Loading example Boston data from SKLearn:\n")
     print(f"Features Shape: {features.shape}")
@@ -36,21 +67,21 @@ def univariate_linear_regression_example():
     for name, feature_set in zip(feature_names, features.transpose()):
         print(f"'{name}':")
 
-        plt.plot(feature_set, labels, "bo")
-        plt.xlabel(name)
-        plt.ylabel("Median House Price ($1000s)")
+        plt.plot(labels, feature_set, "bo")
+        plt.xlabel("Median House Price ($1000s)")
+        plt.ylabel(name)
         plt.savefig(f"./graphs/linear_regression/univariate_example/{name}.png")
 
         best_parameters = ulr_batch_gradient_descent(
             features=feature_set, labels=labels
         )
         print(f"Best Parameters: {best_parameters}")
-        if best_parameters["current_score"] < best_mse_score:
-            best_mse_score = best_parameters["current_score"]
+        if best_parameters["current_mse_score"] < best_mse_score:
+            best_mse_score = best_parameters["current_mse_score"]
             best_feature_name = name
             best_model = UnivariateLinearRegressionModel(
-                gradient=best_parameters["current_gradient"],
-                y_axis_shift=best_parameters["current_y_shift"],
+                theta_0=best_parameters["current_theta_0"],
+                theta_1=best_parameters["current_theta_1"],
             )
         print()
     print(f"Best MSE Score: {best_mse_score}")
