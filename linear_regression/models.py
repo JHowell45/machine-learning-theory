724f2046d19fd757c5b10e6e1c761acf41d7e9b8
JHowell45 <jhowell45@googlemail.com>
2021-04-08 22:39:16 +0100

    added function for getting multiple predictions at once.

--- linear_regression/models.py
+++ linear_regression/models.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass, field
-from pandas import Series
+from pandas import Series, DataFrame
+from typing import List, Union
 
 
 @dataclass
@@ -9,6 +10,23 @@ class UnivariateLinearRegressionModel:
 
     def predict(self, feature: float) -> float:
         return (self.gradient * feature) + self.y_axis_shift
+
+    def multiple_predictions(self, features: Union[Series, List[float]]) -> Series:
+        """Use this function to quickly predict the values for multiple features.
+
+        This function is used for running the linear regression to get the
+        predictions for several features at once.
+
+        :param features: the vector of feature values.
+        :return: the vector of feature predictions.
+        """
+        if isinstance(features, list):
+            features = Series(features)
+        features_dataframe = DataFrame(
+            [Series(1 for _ in range(len(features))), features]
+        ).transpose()
+        params = Series([self.y_axis_shift, self.gradient])
+        return features_dataframe.dot(params)
 
 
 @dataclass
