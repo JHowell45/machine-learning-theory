6b825ac6723bb9282caa93217ee8a405023ec70d
JHowell45 <jhowell45@googlemail.com>
2021-04-18 20:14:54 +0100

    Updated testing.

--- tests/test_linear_regression/test_models.py
+++ tests/test_linear_regression/test_models.py
@@ -9,7 +9,15 @@ from src.linear_regression.models import UnivariateLinearRegressionModel
 class TestUnivariateLinearRegressionModel:
     @pytest.mark.parametrize(
         "theta_0, theta_1,feature,prediction",
-        [[0, 2, 2, 4]],
+        [
+            [0, 2, 2, 4],
+            [2, 2, 2, 6],
+            [2, 5, 2, 12],
+            [0, -5, 2, -10],
+            [7, -5, 2, -3],
+            [17, -5, 2, 7],
+            [-5, 2, 2, -1],
+        ],
     )
     def test_prediction(self, theta_0, theta_1, feature, prediction):
         assert (
